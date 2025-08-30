217e220a2908529bfea2b9bc74fae129f143d509
Lennin Temoche <bigo42923@gmail.com>
2025-08-30 00:10:15 -0500

    se verifica yagrega 1ra comparación

--- 7DaysOfCodeJS/OperacionesBooleanas.js
+++ 7DaysOfCodeJS/OperacionesBooleanas.js
@@ -6,3 +6,14 @@ let stringTreinta = '30';
 
 let numeroDiez = 10;
 let stringDiez = '10';
+
+//1ra Comparación
+// numeroUno == stringUno --> compara solo el valor, (1 == '1') = true
+// y (&&)
+// numeroUno !== stringUno --> compara tipo y valor, si los tipos son distintos da true
+if (numeroUno == stringUno && numeroUno !== stringUno) {
+  console.log('Las variables numeroUno y stringUno tienen el mismo valor, pero tipos diferentes');
+} else {
+  console.log('Las variables numeroUno y stringUno no tienen el mismo valor');
+}
+
