ee7d3bce229bf24bd9599971bb299b8ca0fb651b
Lennin Temoche <bigo42923@gmail.com>
2025-08-31 00:30:01 -0500

    se agregan variables y mensaje test inicial

---
+++ 7DaysOfCodeJS/Dia2/Variables.js
@@ -0,0 +1,7 @@
+// Preguntas al usuario
+let nombre = prompt("¿Cuál es tu nombre?");
+let edad = prompt("¿Cuántos años tienes?");
+let lenguaje = prompt("¿Qué lenguaje de programación estás estudiando?");
+
+// Mensaje:
+alert(`Hola ${nombre}, tienes ${edad} años y ya estás aprendiendo ${lenguaje}!`);
